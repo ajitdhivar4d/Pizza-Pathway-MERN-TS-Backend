0ea8fefa8dd3e19259b737979ff0a35c36638276
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-10 21:23:20 +0530

    update api

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -5,6 +5,7 @@ const clientURL = process.env.CLIENT_URL || "";
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173",
+  "https://pizza-pathway-mern-ts-frontend.vercel.app",
   clientURL,
 ].filter((origin): origin is string => origin !== "" && origin !== undefined);
 
