b36e79def222be00926c80af7c78a83836be8444
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-15 19:18:24 +0530

    update

--- dist/constants/config.js
+++ dist/constants/config.js
@@ -1,12 +1,10 @@
-// const clientURL = process.env.CLIENT_URL || "";
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:4173",
-//   "https://pizza-pathway-mern-ts-frontend.vercel.app",
-//   clientURL,
-// ].filter((origin): origin is string => origin !== "" && origin !== undefined);
+const clientURL = process.env.CLIENT_URL || "";
+const allowedOrigins = [
+    "https://pizza-pathway-mern-ts-frontend.vercel.app",
+    clientURL,
+].filter(Boolean);
 const corsOptions = {
-    origin: "*",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,16 +1,14 @@
 import cors from "cors"; // Import 'cors' and its types
 
-// const clientURL = process.env.CLIENT_URL || "";
+const clientURL: string = process.env.CLIENT_URL || "";
 
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:4173",
-//   "https://pizza-pathway-mern-ts-frontend.vercel.app",
-//   clientURL,
-// ].filter((origin): origin is string => origin !== "" && origin !== undefined);
+const allowedOrigins = [
+  "https://pizza-pathway-mern-ts-frontend.vercel.app",
+  clientURL,
+].filter(Boolean) as string[];
 
 const corsOptions: cors.CorsOptions = {
-  origin: "*",
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
